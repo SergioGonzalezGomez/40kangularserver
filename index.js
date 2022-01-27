9de9cf2142e05eae9191f962a7f886e0bd8def5f
Sergio González Gómez <sergiogonzalezgomez15@gmail.com>
2022-01-27 17:44:27 +0100

    final2

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 })
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:4200', "https://40kangular.vercel.app"],
+    origin: ['http://localhost:3000', 'http://localhost:4200', "https://40kangular.vercel.app", "https://40kangular.vercel.app/legions"],
     credentials: true
 }))
 
