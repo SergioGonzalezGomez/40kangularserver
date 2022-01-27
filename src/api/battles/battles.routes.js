fbc9442a167bc8123a4059f44154f5414b549ba9
Sergio González Gómez <sergiogonzalezgomez15@gmail.com>
2022-01-27 17:07:47 +0100

    serverPush

---
+++ src/api/battles/battles.routes.js
@@ -0,0 +1,13 @@
+const BattleRoutes = require('express').Router()
+const { postNewBattle, getAllBattles, getBattle, patchBattle, deleteBattle } = require('./battles.controller')
+
+
+BattleRoutes.get('/', getAllBattles)
+BattleRoutes.get('/:id', getBattle)
+
+//solo los admins pueden hacer esto
+/* BattleRoutes.post('/', upload.single('img'), postNewBattle)
+BattleRoutes.patch('/:id', upload.single('img'), patchBattle)
+BattleRoutes.delete('/:id', [isAuth], upload.single('img'), deleteBattle) */
+
+module.exports = BattleRoutes
---
+++ src/api/legions/legions.routes.js
@@ -0,0 +1,13 @@
+const LegionRoutes = require('express').Router()
+const { postNewLegion, getAllLegions, getLegion, patchLegion, deleteLegion } = require('./legions.controller')
+
+
+LegionRoutes.get('/', getAllLegions)
+LegionRoutes.get('/:id', getLegion)
+
+//solo los admins pueden hacer esto
+/* LegionRoutes.post('/', upload.single('img'), postNewLegion)
+LegionRoutes.patch('/:id', upload.single('img'), patchLegion)
+LegionRoutes.delete('/:id', [isAuth], upload.single('img'), deleteLegion) */
+
+module.exports = LegionRoutes
